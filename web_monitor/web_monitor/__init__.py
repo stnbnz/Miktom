526f5b2b136408bfdada5cf28bcf491db7639c04
MuhammadSultanBilnadzari <muhammadsultanbilnadzari@gmail.com>
2026-04-13 16:56:07 +0700

    16

--- web_monitor/web_monitor/__init__.py
+++ web_monitor/web_monitor/__init__.py
@@ -1,2 +1,6 @@
 import pymysql
 pymysql.install_as_MySQLdb()
+
+# Django 6.0 requires mysqlclient >= 2.2.1, but PyMySQL reports as 1.4.6
+# Override the version to bypass the check
+pymysql.version_info = (2, 2, 1, 'final', 0)
